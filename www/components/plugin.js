0c36b2b38ed6f822aa6dde05d50bd43902e7147e
millenys4ntos <millenydossantos10.msc@gmail.com>
2021-05-30 00:03:15 +0900

    .

--- www/components/plugin.js
+++ www/components/plugin.js
@@ -1,21 +1,5 @@
 // This is a JavaScript file
 
-$(document).on("click","#camera", function(){
-navigator.camera.getPicture(onSuccess, onFail, {
-   quality: 50,
-    destinationType: Camera.DestinationType.FILE_URI,
-    correctOrientation: true });
-
-function onSuccess(imageURI) {
-    var image = document.getElementById('imagem');
-    image.src = imageURI;
-}
-
-function onFail(message) {
-    alert('Failed because: ' + message);
-}
-});
-
 $(document).on("click","#codigo", function(){
   cordova.plugins.barcodeScanner.scan(
       function (result) {
